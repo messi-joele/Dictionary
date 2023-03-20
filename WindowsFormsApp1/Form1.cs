fe6b84f50c045fe96c5f7bac0993f366d6538efa
messi-joele <messi.nicolajoele.studente@itispaleocapa.it>
2023-03-20 09:43:26 +0100

    nSHBVW

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -33,20 +33,24 @@ namespace WindowsFormsApp1
         {
             Partecipane b1 = new Partecipane(textBox1.Text);
             Importo b2 = new Importo(Convert.ToInt16(textBox2.Text));
-
+            
             accounts.Add(b1, b2);
-
+            
+            
             quota = quota + Convert.ToInt16( textBox2.Text);
             label5.Text = quota.ToString();
             string[] row = { textBox2.Text, textBox1.Text, textBox9.Text};
 
             var ListViewItem = new ListViewItem(row);
             listView1.Items.Add(ListViewItem);
+            textBox1.Clear();
+            textBox2.Clear();
         }
 
         private void del_Click(object sender, EventArgs e)
         {
-           // accounts.Remove(del.Text);
+            Partecipane b3 = new Partecipane(textBox3.Text);
+            accounts.Remove(b3);
            quota = quota - Convert.ToInt16(textBox7.Text);
             label5.Text = quota.ToString();
 
@@ -54,16 +58,24 @@ namespace WindowsFormsApp1
 
         private void btnmod_Click(object sender, EventArgs e)
         {
-          //  accounts.Remove(textBox4.Text);
+            Partecipane b4 = new Partecipane(textBox4.Text);
             quota = quota - Convert.ToInt16(textBox8.Text);
             label5.Text = quota.ToString();
-        //    accounts.Add(textBox5.Text, Convert.ToInt16(textBox6.Text));
+            accounts.Remove(b4);
+            Partecipane b5 = new Partecipane(textBox5.Text);
+            Importo b3 = new Importo(Convert.ToInt16(textBox6.Text));
+            accounts.Add(b5,b3);
             quota = quota + Convert.ToInt16(textBox6.Text);
             label5.Text = quota.ToString();
-            string[] row = { textBox5.Text, textBox6.Text };
+            string[] row = { textBox6.Text, textBox5.Text };
 
             var ListViewItem = new ListViewItem(row);
             listView1.Items.Add(ListViewItem);
+            textBox4.Clear();
+            textBox8.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+
         }
     }
 }
